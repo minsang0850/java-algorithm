57a7f9d6927308cde8842c3c1756e0beb8932570
mingsang <minsang0850@naver.com>
2024-04-20 18:20:32 +0900

    에러 수정

---
+++ src/main/java/programmers/introduction/최빈값_구하기.java
@@ -0,0 +1,38 @@
+package programmers.introduction;
+
+public class 최빈값_구하기 {
+
+    static int[] recordArray;
+
+    public int solution(int[] array) {
+        init();
+        int answer = 0;
+        int maxCalled = 0;
+        for(int number : array) {
+            recordArray[number] ++;
+        }
+        for(int i =0; i<1001; i++) {
+            if(maxCalled<recordArray[i]){
+                maxCalled=recordArray[i];
+            }
+        }
+        int duplicateAnswer = 0;
+        for(int i =0; i<array.length; i++) {
+            if(maxCalled==recordArray[i]){
+                answer = array[i];
+                duplicateAnswer++;
+            }
+        }
+        if(duplicateAnswer>1){
+            return -1;
+        }
+        return answer;
+    }
+
+    private void init() {
+        recordArray = new int[1001];
+        for(int i=0; i<1001; i++){
+            recordArray[i]=0;
+        }
+    }
+}
