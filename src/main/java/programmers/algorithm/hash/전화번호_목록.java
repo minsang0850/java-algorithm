0435a06c0ddc80afc16bfc422ed22256efb276ca
mingsang <minsang0850@naver.com>
2024-04-21 15:53:40 +0900

    프로그래머스: 전화번호 목록

---
+++ src/main/java/programmers/algorithm/hash/전화번호_목록.java
@@ -0,0 +1,18 @@
+package programmers.algorithm.hash;
+
+import java.util.Arrays;
+
+public class 전화번호_목록 {
+    public boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book);
+        if(phone_book.length<2){
+            return true;
+        }
+        for (int i =0; i<phone_book.length-1; i++) {
+            if(phone_book[i+1].startsWith(phone_book[i])){
+                return false;
+            }
+        }
+        return true;
+    }
+}
