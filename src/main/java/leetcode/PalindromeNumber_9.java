0b3c1895259e0b005e5c6c62435c3c833b921c79
박민상 <minsang0850@naver.com>
2024-09-20 20:44:49 +0900

    leetcode: palindrome number

--- src/main/java/leetcode/PalindromeNumber_9.java
+++ src/main/java/leetcode/PalindromeNumber_9.java
@@ -3,7 +3,7 @@ package leetcode;
 public class PalindromeNumber_9 {
     public boolean isPalindrome(int x) {
         var string = String.valueOf(x);
-        for (int i=0; i<string.length(); i++) {
+        for (int i=0; i<string.length()/2; i++) {
             if (string.charAt(i) != string.charAt(string.length()-1-i)) {
                 return false;
             }
