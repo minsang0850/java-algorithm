added3b406ff082b15dd211977c87ac439ed6180
박민상 <minsang0850@naver.com>
2024-10-16 19:30:11 +0900

    leetcode: merge strings alternmately

--- src/main/java/leetcode/MergeStringsAlternately_1768.java
+++ src/main/java/leetcode/MergeStringsAlternately_1768.java
@@ -3,12 +3,12 @@ package leetcode;
 public class MergeStringsAlternately_1768 {
     public String mergeAlternately(String word1, String word2) {
         var sb = new StringBuilder();
-        var i = 0;
-        var j = 0;
-        while (i < word1.length() && j < word2.length()) {
-            sb.append(word1.charAt(i++));
-            sb.append(word2.charAt(j++));
+        var firstIdx = 0;
+        var secondIdx = 0;
+        while (firstIdx < word1.length() && secondIdx < word2.length()) {
+            sb.append(word1.charAt(firstIdx++));
+            sb.append(word2.charAt(secondIdx++));
         }
-        return sb.append(word1.substring(i)).append(word2.substring(j)).toString();
+        return sb.append(word1.substring(firstIdx)).append(word2.substring(secondIdx)).toString();
     }
 }
