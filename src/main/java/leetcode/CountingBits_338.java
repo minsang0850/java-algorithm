d1bc4fd74c0b5afd7330a8fd12a805f5b86557b6
mingsang <minsang0850@naver.com>
2024-10-27 15:03:37 +0900

    leetcode: counting bits

--- src/main/java/leetcode/CountingBits_338.java
+++ src/main/java/leetcode/CountingBits_338.java
@@ -3,24 +3,9 @@ package leetcode;
 public class CountingBits_338 {
     public int[] countBits(int n) {
         var answer = new int[n+1];
-        if(n==0) {
-            return answer;
-        }
-        answer[1]=1;
-        for (int i=2; i<n+1; i++) {
-            var finded = find(i);
-            answer[i]=answer[i-finded] + 1;
+        for (int i=1; i<n+1; i++) {
+            answer[i]=answer[i>>1] + i%2;
         }
         return answer;
     }
-
-    private int find(int n) {
-        for(int i=1; i<18; i++) {
-            if(n<Math.pow(2, i)) {
-                return (int)Math.pow(2, i-1);
-            }
-        }
-        return -1;
-    }
-
 }
