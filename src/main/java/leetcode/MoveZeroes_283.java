1e2adb7f9b50bd0427fdf87c88dffc84e29da7fb
mingsang <minsang0850@naver.com>
2024-11-01 19:11:14 +0900

    leetcode: string compression

--- src/main/java/leetcode/MoveZeroes_283.java
+++ src/main/java/leetcode/MoveZeroes_283.java
@@ -1,2 +1,23 @@
-package leetcode;public class MoveZeroes_283 {
+package leetcode;
+
+import java.util.Arrays;
+
+public class MoveZeroes_283 {
+    public void moveZeroes(int[] nums) {
+        var k = nums.length-1;
+
+        for (int i=0; i<nums.length; i++) {
+            if(nums[i]==0) {
+                while(k>0 && nums[k]==0) {
+                    k--;
+                }
+                if(k>i) {
+                    nums[i]=nums[k];
+                    nums[k]=0;
+                }
+            }
+        }
+
+        Arrays.sort(nums, 0, k+1);
+    }
 }
