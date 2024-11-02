b507505a413b3e28491c2bf80e4e919aeaa4d3ea
mingsang <minsang0850@naver.com>
2024-11-02 19:14:39 +0900

    leetcode: move zeroes

--- src/main/java/leetcode/MoveZeroes_283.java
+++ src/main/java/leetcode/MoveZeroes_283.java
@@ -4,20 +4,21 @@ import java.util.Arrays;
 
 public class MoveZeroes_283 {
     public void moveZeroes(int[] nums) {
-        var k = nums.length-1;
+        var k = 0;
 
         for (int i=0; i<nums.length; i++) {
+            k=Math.max(k, i);
             if(nums[i]==0) {
-                while(k>0 && nums[k]==0) {
-                    k--;
+                while(k<nums.length && nums[k]==0) {
+                    k++;
                 }
-                if(k>i) {
+
+                if(k<nums.length) {
                     nums[i]=nums[k];
                     nums[k]=0;
                 }
             }
         }
 
-        Arrays.sort(nums, 0, k+1);
     }
 }
