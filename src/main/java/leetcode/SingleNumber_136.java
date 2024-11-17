7a3131758c83ed3af17f3f9d62224262545294de
mingsang <minsang0850@naver.com>
2024-11-18 01:15:51 +0900

    leetcode: single number

---
+++ src/main/java/leetcode/SingleNumber_136.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+public class SingleNumber_136 {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
+
+}
